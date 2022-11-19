ce5d7f6ed5c659d07feba550fe0eafcb25e79337
Gema Parreño <gema.parreno.piqueras@gmail.com>
2022-11-19 12:27:33 +0100

    Stage load data

--- ExperimentsDVC/src/data/load_dataset.py
+++ ExperimentsDVC/src/data/load_dataset.py
@@ -1,8 +1,15 @@
 # Functions that load the datasets
 import pandas as pd
+import yaml
 
-def read_data(path):
+
+
+with open('params.yaml') as config_file:
+    config = yaml.safe_load(config_file)
+
+def read_data():
     """Reads the data"""
-    data = pd.read_csv(path)
+    data_path_var = config_file['load']['data_path']
+    data = pd.read_csv(data_path_var)
     print('----DATASET LOADED----')
     return data
