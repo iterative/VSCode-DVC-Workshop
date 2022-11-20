e4767ddb78b6a9d432ca85f98693850524e4d288
Gema Parreño <gema.parreno.piqueras@gmail.com>
2022-11-20 12:13:11 +0100

    update params

--- ExperimentsDVC/src/features/feature_engineering.py
+++ ExperimentsDVC/src/features/feature_engineering.py
@@ -7,10 +7,10 @@ with open('params.yaml') as config_file:
     config = yaml.safe_load(config_file)
 
 
-def feature_selection(data):
+def feature_selection(data,list_column_x, list_column_y):
     """Select features for training and target features"""
-    X = data[config['featureselection']['features']]
-    y = data[config['featureselection']['labels']]
+    X = data[list_column_x]
+    y = data[list_column_y]
     print('----FEATURES SELECTED----')
     return X , y
 
