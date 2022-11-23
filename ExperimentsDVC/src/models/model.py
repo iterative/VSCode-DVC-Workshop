eaa5dd7817386fd15b0138419fbf88de34581660
Gema Parreño <gema.parreno.piqueras@gmail.com>
2022-11-23 15:49:14 +0100

    end-to-end fixes

--- ExperimentsDVC/src/models/model.py
+++ ExperimentsDVC/src/models/model.py
@@ -4,11 +4,11 @@ import yaml
 with open('params.yaml') as config_file:
     config = yaml.safe_load(config_file)
 
-def RandomForestModel(n_estimators,max_depth,min_samples_split,min_impurity_decrease ):
+def RandomForestModel(n_estimators,max_samples,max_depth,min_samples_split,min_impurity_decrease ):
     """Fits the classifier"""
     regressor = RandomForestRegressor( bootstrap=True,
-        max_samples= max_samples,
         n_estimators = n_estimators,
+        max_samples= max_samples,
         max_depth=max_depth,
         min_samples_split=min_samples_split,
         min_impurity_decrease=min_impurity_decrease)
--- ExperimentsDVC/src/models/train_model.py
+++ ExperimentsDVC/src/models/train_model.py
@@ -9,6 +9,20 @@ import pandas as pd
 
 # One Stage pipeline. This script will be executed with 'dvc exp run' or 'dvc repro'
 
+##### FIX THE TABLE ISSUE ? ? ? 
+
+
+####----------------######
+
+import sys
+from pathlib import Path
+
+src_path = Path(__file__).parent.parent.resolve()
+sys.path.append(str(src_path))
+
+####----------------######
+
+
 with open('params.yaml') as config_file:
     config = yaml.safe_load(config_file)
 
@@ -34,14 +48,15 @@ X_data_transformed_test = standard_scaler(X_test)
 ### Train setup 
 live = Live()
 model = RandomForestModel(
-    n_estimators = config['train']['n_estimators'],
+    n_estimators=config['train']['n_estimators'],
+    max_samples= config['train']['max_samples'],
     max_depth=config['train']['max_depth'],
     min_samples_split=config['train']['min_samples_split'],
     min_impurity_decrease=config['train']['min_impurity_decrease'])
 
 
 
-#with Live() as live:
+
 print('----TRAINING----')
 model.fit(X_data_transformed, y_train)
 print('----TRAINING DONE----')
@@ -55,9 +70,17 @@ validation_data = read_data(config['load']['data_path_test'])
 transform_to_datetime(validation_data, 'epoch')
 delta_time(validation_data,'epoch')
 
-validation_data = validation_data.apply(pd.to_numeric, errors='ignore')
+validation_data.info()
 
+validation_data = validation_data.drop(columns=['epoch'])
+
+validation_data.info()
+print()
+
+validation_data = validation_data.apply(pd.to_numeric, errors='ignore')
 X_validation_transformed = standard_scaler(validation_data)
+
+
 
 print(model.predict(X_validation_transformed))
 
